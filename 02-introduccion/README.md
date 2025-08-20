4d11974c3755e6b87b2c5d263be0e907a91adf0b
Blue Feather Dev <jesusdominguez2004@gmail.com>
2025-08-19 23:13:24 -0500

    introducción: notas personales

---
+++ 02-introduccion/README.md
@@ -0,0 +1,13 @@
+## 🔗 Enlaces de interés
+
+### API y FastAPI
+
+- [FastAPI](https://fastapi.tiangolo.com/)
+- [OpenAPI](https://github.com/OAI/OpenAPI-Specification)
+- [JSON Schema](httphttps://json-schema.org/)
+
+### Type Hints
+
+- [Introducción a Tipos en Python](https://fastapi.tiangolo.com/es/python-types/)
+
+> Made with '\u{2665}' (♥)
---
+++ 02-introduccion/type_hints.py
@@ -0,0 +1,17 @@
+# Type hints
+
+string_variable = "Hello, Python!"
+print(string_variable) # Hello, Python!
+print(type(string_variable)) # <class 'str'>
+
+string_variable = 16
+print(string_variable) # 16
+print(type(string_variable)) # <class 'int'>
+
+typed_string_variable: str = "Hello, FastAPI!"
+print(typed_string_variable) # Hello, FastAPI!
+print(type(typed_string_variable)) # <class 'str'>
+
+typed_string_variable = 23
+print(typed_string_variable) # 23
+print(type(typed_string_variable)) # <class 'int'>
